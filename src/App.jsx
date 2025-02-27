b2845056899aa31b3cc30d7548a9b0e61fa3b2a8
sajid-edninja <ssk73573@gmail.com>
2025-02-27 18:52:58 +0530

    add counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,36 @@
-
+import { useState } from "react";
 
 function App() {
-  return <div>
-    hi there
-  </div>
+  return (
+    <div>
+      <b>hi there</b>
+      <Counter></Counter>
+    </div>
+  );
 }
 
-export default App
+function Counter() {
+  const [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+  function resetCount() {
+    setCount(0);
+  }
+
+  return (
+    <div>
+      <h1 id="text">{count}</h1>
+      <button onClick={increaseCount}>Increase count</button>
+      <button onClick={decreaseCount}>Decrease count</button>
+      <button onClick={resetCount}>Reset count</button>
+    </div>
+  );
+}
+
+export default App;
