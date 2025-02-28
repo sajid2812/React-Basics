59d1eeda110b3fc93cc972067541cad40c2f96ae
sajid-edninja <ssk73573@gmail.com>
2025-02-28 20:43:50 +0530

    add time using useRef

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
-import { useRef } from "react";
+import { useState, useRef } from "react";
 
 function App() {
-  const inputRef = useRef();
-  function focusInput() {
-    inputRef.current.focus();
+  const [count, setCount] = useState(1);
+  const timer = useRef();
+
+  function startClock() {
+    timer.current = setInterval(() => {
+      setCount((c) => c + 1);
+    }, 1000);
+  }
+  function endClock() {
+    clearInterval(timer.current);
   }
 
   return (
     <div>
-      Signup
-      <input ref={inputRef} id="name" type="text" />
-      <button onClick={focusInput}>Submit</button>
+      {count}
+      <br />
+      <button onClick={startClock}>Start</button>
+      <button onClick={endClock}>Stop</button>
     </div>
   );
 }
