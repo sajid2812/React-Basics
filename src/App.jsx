a397737289f10be4214cc62a50109c4af87fafce
sajid-edninja <ssk73573@gmail.com>
2025-03-01 22:59:04 +0530

    add custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { useState } from "react";
-
-function useCounter() {
-  const [count, setCount] = useState(0);
-  function increaseCount() {
-    setCount((count) => count + 1);
-  }
-  return { count, increaseCount };
-}
+import { useFetch } from "./useFetch.js";
 
 function App() {
-  const { count, increaseCount } = useCounter();
+  const [currentPost, setCurrentPost] = useState(1);
+  const { finalData } = useFetch(
+    "https://jsonplaceholder.typicode.com/todos/" + currentPost
+  );
   return (
     <div>
-      <button onClick={increaseCount}>Increase {count}</button>
+      <button onClick={() => setCurrentPost(1)}>1</button>
+      <button onClick={() => setCurrentPost(2)}>2</button>
+      <button onClick={() => setCurrentPost(3)}>3</button>
+      {JSON.stringify(finalData)}
     </div>
   );
 }
---
+++ src/useFetch.js
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+
+export function useFetch(url) {
+  const [finalData, setFinalData] = useState({});
+
+  async function getDetails() {
+    const response = await fetch(url);
+    const json = await response.json();
+    setFinalData(json);
+  }
+
+  useEffect(() => {
+    getDetails();
+  }, [url]);
+
+  return {
+    finalData,
+  };
+}
