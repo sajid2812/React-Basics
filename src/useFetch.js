1c2bc07147504919c5861a84ad24ad170b686e38
sajid-edninja <ssk73573@gmail.com>
2025-03-02 09:21:47 +0530

    modify useFetch func

--- src/useFetch.js
+++ src/useFetch.js
@@ -3,6 +3,7 @@ import { useEffect, useState } from "react";
 export function useFetch(url) {
   const [finalData, setFinalData] = useState({});
   const [loading, setLoading] = useState(false);
+  const [timer, setTimer] = useState(0);
 
   async function getDetails() {
     setLoading(true);
@@ -15,6 +16,15 @@ export function useFetch(url) {
   useEffect(() => {
     getDetails();
   }, [url]);
+
+  useEffect(() => {
+    const timer = setInterval(getDetails, 10 * 1000);
+    setTimer(timer);
+
+    return function () {
+      clearInterval(timer);
+    };
+  }, []);
 
   return {
     finalData,
