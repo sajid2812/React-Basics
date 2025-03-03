9a5228e4192c4b5c4c04ffa6445dc25ec9756d24
sajid-edninja <ssk73573@gmail.com>
2025-03-03 09:48:30 +0530

    add selector

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,6 @@
 import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { counterAtom } from "../src/store/atoms/counter.js";
 
-function App() {
-  return (
-    <RecoilRoot>
-      <Counter />
-    </RecoilRoot>
-  );
-}
 
-function Counter() {
-  return (
-    <div>
-      <CurrentCount />
-      <Increase />
-      <Decrease />
-    </div>
-  );
-}
-
-function CurrentCount() {
-  const count = useRecoilValue(counterAtom);
-  return <div>{count}</div>;
-}
-
-function Increase() {
-  const setCount = useSetRecoilState(counterAtom);
-  function increase() {
-    setCount((count) => count + 1);
-  }
-  return <button onClick={increase}>+</button>;
-}
-
-function Decrease() {
-  const setCount = useSetRecoilState(counterAtom);
-  function decrease() {
-    setCount((count) => count - 1);
-  }
-  return <button onClick={decrease}>-</button>;
-}
 
 export default App;
--- src/store/atoms/counter.js
+++ src/store/atoms/counter.js
@@ -1,6 +1,14 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
 
 export const counterAtom = atom({
   key: "counter",
   default: 0,
+});
+
+export const even = selector({
+  key: "isEvenSelector",
+  get: ({ get }) => {
+    const currCount = get(counterAtom);
+    return currCount % 2 === 0;
+  },
 });
