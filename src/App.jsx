d6b2f40ae8e4b900aa9f4000f6f5b091084aaeff
sajid-edninja <ssk73573@gmail.com>
2025-03-03 20:40:19 +0530

    fix notification atom

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { RecoilRoot, useRecoilValue, useSetRecoilState } from "recoil";
 import { counterAtom, evenSelector } from "../src/store/atoms/counter.js";
 
 function App() {
+  const networkNotificationAtom = useRecoilValue(networkAtom);
+
   return (
     <div>
       <RecoilRoot>
@@ -11,35 +13,6 @@ function App() {
       </RecoilRoot>
     </div>
   );
-}
-
-function Buttons() {
-  const setCount = useSetRecoilState(counterAtom);
-
-  function increase() {
-    setCount((c) => c + 2);
-  }
-
-  function decrease() {
-    setCount((c) => c - 1);
-  }
-
-  return (
-    <div>
-      <button onClick={increase}>Increase</button>
-      <button onClick={decrease}>Decrease</button>
-    </div>
-  );
-}
-
-function Counter() {
-  const count = useRecoilValue(counterAtom);
-  return <div>{count}</div>;
-}
-
-function IsEven() {
-  const even = useRecoilValue(evenSelector);
-  return <div>{even ? "Even" : "Odd"}</div>;
 }
 
 export default App;
--- src/store/atoms/counter.js
+++ src/store/atoms/counter.js
@@ -1,14 +1,24 @@
 import { atom, selector } from "recoil";
 
-export const counterAtom = atom({
-  key: "counter",
-  default: 0,
+export const notifications = atom({
+  key: "networkAtom",
+  default: {
+    network: 4,
+    jobs: 6,
+    messaging: 3,
+    notifications: 3,
+  },
 });
 
-export const evenSelector = selector({
-  key: "isEvenSelector",
+export const totalNotificationSelector = selector({
+  key: "totalNotificationSelector",
   get: ({ get }) => {
-    const currCount = get(counterAtom);
-    return currCount % 2 === 0;
+    const allNotifications = get(notifications);
+    return (
+      allNotifications.network +
+      allNotifications.jobs +
+      allNotifications.messaging +
+      allNotifications.notifications
+    );
   },
 });
